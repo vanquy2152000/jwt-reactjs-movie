dff590f784afb33db91bc01a86e72496a323e31f
Cao Van Quy <quy.187pm20553@vanlanguni.vn>
2022-12-20 17:31:24 +0700

    fix sidebar

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -78,7 +78,9 @@ const Sidebar = ({ open, toggleSidebar }) => {
                 )}
 
                 <Typography variant="h6" marginBottom="20px">THEME</Typography>
-                <ListItemButton>
+                <ListItemButton
+                    onClick={onSwitchTheme}
+                >
                     <ListItemIcon>
                         {themeMode === themeModes.dark && <DarkModeOutlinedIcon />}
                         {themeMode === themeModes.light && <WbSunnyOutlinedIcon />}
